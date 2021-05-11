549de9e5e9d204274ba7ecab84792c7e5c3ce99b
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 17:42:45 +0100

    Added answer

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import numpy as np
 x = np.loadtxt("data.dat")
 
 # Your code will go here
-
-
-
+dmin = min(x)
+lowq = np.percentile( x, 25 )
+median = np.percentile( x, 50 )
+highq = np.percentile( x, 75 )
+dmax = max(x)
 
 # This will produce a box plot for you automatically
 plt.boxplot( x )
