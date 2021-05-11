2a014662a681b9c6a3fe2a6fe51ef1bdee78df30
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 17:43:00 +0100

    Fixed tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -15,18 +15,18 @@ class UnitTests(unittest.TestCase) :
         myx = np.loadtxt("data.dat")
         assert( vc.check_vars("dmin",min(myx)) )
 
-    def test_dmin(self) : 
+    def test_lowq(self) : 
         myx = np.loadtxt("data.dat")
         assert( vc.check_vars("lowq",np.percentile(myx,25)) )
 
-    def test_dmin(self) : 
+    def test_median(self) : 
         myx = np.loadtxt("data.dat")
         assert( vc.check_vars("median",np.percentile(myx,50)) )
 
-    def test_dmin(self) : 
+    def test_highq(self) : 
         myx = np.loadtxt("data.dat")
         assert( vc.check_vars("highq",np.percentile(myx,75)) )
 
-    def test_dmin(self) : 
+    def test_dmax(self) : 
         myx = np.loadtxt("data.dat")
         assert( vc.check_vars("dmax",max(myx)) )
